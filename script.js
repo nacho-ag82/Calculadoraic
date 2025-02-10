22ad01ced80552c70890131b5bf491326409b513
nacho-ag82 <nachoag82@gmail.com>
2025-02-10 11:15:40 +0100

    practica de hoy

---
+++ script.js
@@ -0,0 +1,20 @@
+function suma(a, b) {
+    return a + b;
+    }
+    function resta(a, b) {
+    return a - b;
+    }
+    function multiplicar(a, b) {
+    return a * b;
+    }
+    function dividir(a, b) {
+    if (b === 0) {
+    throw new Error("No se puede dividir por cero");
+    }
+    return a / b;
+    }
+    // Hacer las funciones accesibles en el navegador
+    window.suma = suma;
+    window.resta = resta;
+    window.multiplicar = multiplicar;
+    window.dividir = dividir;
